a5b16b58c4c5cb321a52992a7470d334540e9464
cxim1 <cxim1@kali.cxim>
2020-06-08 05:05:16 +0300

    for fun

---
+++ get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/cxim1/get_next_line/get_next_line.c" "/home/cxim1/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/get_next_line.c.o"
+  "/home/cxim1/get_next_line/get_next_line_utils.c" "/home/cxim1/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/get_next_line_utils.c.o"
+  "/home/cxim1/get_next_line/gnl_tester.c" "/home/cxim1/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/gnl_tester.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
